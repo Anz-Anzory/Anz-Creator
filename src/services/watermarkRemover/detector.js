1d769d3a532fe065033cf2f21e3303b889e63ad8
Anz-Anzory <anzoryputra4@gmail.com>
2026-04-09 01:47:45 +0700

    Implement watermark detection function

---
+++ src/services/watermarkRemover/detector.js
@@ -0,0 +1,40 @@
+import fs from "fs";
+import { PNG } from "pngjs";
+import pixelmatch from "pixelmatch";
+
+export function detectWatermark(framePath, templatePath) {
+  const img = PNG.sync.read(fs.readFileSync(framePath));
+  const template = PNG.sync.read(fs.readFileSync(templatePath));
+
+  const { width: tw, height: th } = template;
+
+  let best = { x: 0, y: 0, diff: Infinity };
+
+  for (let y = 0; y < img.height - th; y += 10) {
+    for (let x = 0; x < img.width - tw; x += 10) {
+      const crop = new PNG({ width: tw, height: th });
+
+      PNG.bitblt(img, crop, x, y, tw, th, 0, 0);
+
+      const diff = pixelmatch(
+        crop.data,
+        template.data,
+        null,
+        tw,
+        th,
+        { threshold: 0.2 }
+      );
+
+      if (diff < best.diff) {
+        best = { x, y, diff };
+      }
+    }
+  }
+
+  return {
+    x: best.x,
+    y: best.y,
+    width: tw,
+    height: th
+  };
+}
