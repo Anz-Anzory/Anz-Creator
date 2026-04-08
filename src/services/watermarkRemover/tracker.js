4b9210ef27e9557935884416fd468100904d2c93
Anz-Anzory <anzoryputra4@gmail.com>
2026-04-09 01:48:20 +0700

    Implement basic watermark tracking function
    Add a basic watermark tracking function with comments for future enhancements.

---
+++ src/services/watermarkRemover/tracker.js
@@ -0,0 +1,7 @@
+export function trackWatermark(prev, frameIndex) {
+  // simple static (watermark biasanya diam)
+  return prev;
+
+  // kalau mau advanced:
+  // bisa tambah optical flow / motion tracking
+}
