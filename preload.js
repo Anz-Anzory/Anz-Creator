b6939d6fc182c05f141212b521ea701b89564da2
Anz-Anzory <anzoryputra4@gmail.com>
2026-04-09 20:43:52 +0700

    Implement IPC channel whitelist for security
    Added IPC channel validation for security in context isolation.

--- preload.js
+++ preload.js
@@ -1,9 +1,35 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
+// FIX: Daftar whitelist saluran IPC yang diizinkan untuk keamanan Context Isolation
+const validChannels = [
+  'save-api-keys',
+  'get-key-status',
+  'get-gemini-stats',
+  'analyze-video',
+  'detect-watermark',
+  'remove-watermark',
+  'split-long-video',
+  'get-video-info',
+  'select-output-dir'
+];
+
 contextBridge.exposeInMainWorld('electron', {
   ipcRenderer: {
-    invoke: (channel, data) => ipcRenderer.invoke(channel, data),
-    on: (channel, callback) => ipcRenderer.on(channel, (event, ...args) => callback(...args)),
-    removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel)
+    invoke: (channel, data) => {
+      if (validChannels.includes(channel)) {
+        return ipcRenderer.invoke(channel, data);
+      }
+      return Promise.reject(new Error(`Akses IPC ditolak untuk saluran tidak sah: ${channel}`));
+    },
+    on: (channel, callback) => {
+      if (validChannels.includes(channel)) {
+        ipcRenderer.on(channel, (event, ...args) => callback(...args));
+      }
+    },
+    removeAllListeners: (channel) => {
+      if (validChannels.includes(channel)) {
+        ipcRenderer.removeAllListeners(channel);
+      }
+    }
   }
 });
