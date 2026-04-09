d46de70ef1a60b19f1b949ed72b78f63ff4b2801
Anz-Anzory <anzoryputra4@gmail.com>
2026-04-09 20:46:03 +0700

    Use Link components for navigation in App.jsx
    Replaced anchor tags with Link components to prevent unintended reloads.

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Editor from './pages/Editor';
 import Settings from './pages/Settings';
@@ -12,9 +12,10 @@ function App() {
         <nav className="navbar">
           <h1>Anz Video Publisher</h1>
           <div className="nav-links">
-            <a href="/">Home</a>
-            <a href="/#/editor">Editor</a>
-            <a href="/#/settings">Settings</a>
+            {/* FIX: Mengganti tag <a> menjadi komponen <Link> mencegah reload yang tidak disengaja */}
+            <Link to="/">Home</Link>
+            <Link to="/editor">Editor</Link>
+            <Link to="/settings">Settings</Link>
           </div>
         </nav>
         
