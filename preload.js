fce0086c496bb3eafe30a14605a4c9c3f76f1883
Anz-Anzory <anzoryputra4@gmail.com>
2026-04-09 19:31:27 +0700

    Update preload.js

--- preload.js
+++ preload.js
@@ -3,7 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electron', {
   ipcRenderer: {
     invoke: (channel, data) => ipcRenderer.invoke(channel, data),
-    on: (channel, callback) => ipcRenderer.on(channel, callback),
+    on: (channel, callback) => ipcRenderer.on(channel, (event, ...args) => callback(...args)),
     removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel)
   }
 });
