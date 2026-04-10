d414c197e03f75478e327cc9ca2b6e95e850ddbe
Anz-Anzory <anzoryputra4@gmail.com>
2026-04-10 18:18:33 +0700

    Update detector.js

--- src/services/watermarkRemover/detector.js
+++ src/services/watermarkRemover/detector.js
@@ -3,40 +3,45 @@ const { PNG } = require("pngjs");
 const pixelmatch = require("pixelmatch");
 
 function detectWatermark(framePath, templatePath) {
-  const img = PNG.sync.read(fs.readFileSync(framePath));
-  const template = PNG.sync.read(fs.readFileSync(templatePath));
+  try {
+    const img = PNG.sync.read(fs.readFileSync(framePath));
+    const template = PNG.sync.read(fs.readFileSync(templatePath));
 
-  const { width: tw, height: th } = template;
+    const { width: tw, height: th } = template;
 
-  let best = { x: 0, y: 0, diff: Infinity };
+    let best = { x: 0, y: 0, diff: Infinity };
 
-  for (let y = 0; y < img.height - th; y += 10) {
-    for (let x = 0; x < img.width - tw; x += 10) {
-      const crop = new PNG({ width: tw, height: th });
+    for (let y = 0; y < img.height - th; y += 10) {
+      for (let x = 0; x < img.width - tw; x += 10) {
+        const crop = new PNG({ width: tw, height: th });
 
-      PNG.bitblt(img, crop, x, y, tw, th, 0, 0);
+        PNG.bitblt(img, crop, x, y, tw, th, 0, 0);
 
-      const diff = pixelmatch(
-        crop.data,
-        template.data,
-        null,
-        tw,
-        th,
-        { threshold: 0.2 }
-      );
+        const diff = pixelmatch(
+          crop.data,
+          template.data,
+          null,
+          tw,
+          th,
+          { threshold: 0.2 }
+        );
 
-      if (diff < best.diff) {
-        best = { x, y, diff };
+        if (diff < best.diff) {
+          best = { x, y, diff };
+        }
       }
     }
-  }
 
-  return {
-    x: best.x,
-    y: best.y,
-    width: tw,
-    height: th
-  };
+    return {
+      x: best.x,
+      y: best.y,
+      width: tw,
+      height: th
+    };
+  } catch (err) {
+    console.error('Watermark detection error:', err.message);
+    return null;
+  }
 }
 
 module.exports = {
